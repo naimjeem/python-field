abef1ef0db7b863823982af8f573fc18354798da
Naim Jeem <naim36208@gmail.com>
2018-05-16 23:54:16 +0600

    Conditional statement

---
+++ basic/conditional.py
@@ -0,0 +1,15 @@
+import sys
+
+# N = int(input().strip())
+print('Type a number: ')
+n = int(input().strip())
+
+if (n % 2) != 0:
+  print("Weird")
+else:
+  if (2 <= n) and (n <= 5):
+    print("Not Weird")
+  elif (6 <= n) and (n <= 20):
+    print("Weird")
+  else:
+    print("Not Weird")
