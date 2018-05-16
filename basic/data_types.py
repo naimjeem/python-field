a9e35461a965fe40226ef37ad42e9ecc16c763d0
Naim Jeem <naim36208@gmail.com>
2018-05-16 22:22:18 +0600

    Data types

---
+++ basic/data_types.py
@@ -0,0 +1,23 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Declare second integer, double, and String variables.
+print('Integer--')
+ii = int(input())
+
+print('Float--')
+dd = float(input())
+
+print('String--')
+ss = input()
+
+# Print the sum of both integer variables on a new line.
+print(i + ii)
+
+# Print the sum of the double variables on a new line.
+print(d + dd)
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + ss)
